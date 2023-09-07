3bcd17686eb20dacdfd2748987d0618adec09341
Tanner McDaniel <tmmcdaniel909@gmail.com>
2023-09-07 23:17:59 +0000

    Corrected syntax with bob dos

--- main.js
+++ main.js
@@ -8,29 +8,29 @@ function randomValueFromArray(array){
 }
 
 
-const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
-const insertY = ["the soup kitchen", "Disneyland", "the White House"]
-const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+var insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+var insertY = ["the soup kitchen", "Disneyland", "the White House"];
+var insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 randomize.addEventListener('click', result);
 
 function result() {
-  const newStory = storyText
-  const xItem = randomValueFromArray(insertX);
-  const yItem = randomValueFromArray(insertY);
-  const zItem = randomValueFromArray(insertZ);
-  newStory = newStory.replaceAll(insertX, xItem);
-  newStory = newStory.replaceAll(insertY, yItem);
-  newStory = newStory.replaceAll(insertZ, zItem);
+  var newStory = storyText;
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
+  newStory = newStory.replaceAll(":insertx:", xItem);
+  newStory = newStory.replaceAll(":inserty:", yItem);
+  newStory = newStory.replaceAll(":insertz:", zItem);
   if(customName.value !== '') {
-    const name = customName.value;
+    var name = customName.value;
     newStory.replaceAll("Bob", name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = "Math.round(300 * 0.071429) stone";
-    const temperature =  "Math.round((94-32) * (5/9)) centigrade";
+    var weight = Math.round(300 * 0.071429)+" stone";
+    var temperature =  Math.round((94-32) * (5/9))+" centigrade";
     newStory = newStory.replaceAll("94 fahrenheit", temperature);
     newStory = newStory.replaceAll("300 pounds", weight);
   }
